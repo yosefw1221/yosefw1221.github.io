7250191dcdf6079ba9d605fec143f137bf89cab9
Yosef <yosefworku1221@gmail.com>
2023-01-01 14:54:05 +0300

    personal-site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import Hero from "../components/Hero";
+import Skills from "../components/Skills";
+import About from "../components/About";
+import Works from "../components/Works";
+import Experiences from "../components/Experiences";
+import { useEffect } from "react";
+import useElementObserver from "../hook/useElementObserver";
 
 export default function Home() {
+  const [size, refs, active] = useElementObserver();
+  useEffect(() => { size(5) }, [])
+  
   return (
-    <div className={styles.container}>
+    <div
+      style={{ scrollBehavior: "smooth" }}
+      className="min-h-screen font-mono flex flex-col bg-[#1a191d] scroll-smooth selection:bg-[#37e5] selection:text-white"
+    >
       <Head>
         <title>Yosefw | FullStack Developer</title>
-        <meta name="description" content="FullStack web and mobile app developer" />
+        <meta
+          name="description"
+          content="FullStack web and mobile app developer"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-       
+      <main className="flex-1">
+        <NavBar active={active} />
+        <Hero innerRef={refs[0]} />
+        <About innerRef={refs[1]} />
+        <Skills innerRef={refs[2]}/>
+        <Works innerRef={refs[3]}/>
+        <Experiences innerRef={refs[4]}/>
       </main>
+      <Footer />
     </div>
-  )
+  );
 }
