ec2a3a26473c7ccc9fe0999db97ce26c686bdd8e
Yosef <yosefworku1221@gmail.com>
2023-01-02 22:51:00 +0300

    add viewport meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ export default function Home() {
     >
       <Head>
         <title>Yosefw | FullStack Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
           content="FullStack web and mobile app developer"
