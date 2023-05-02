62918c5819ea1638a5220454a1bb76f3121d0ca5
Yosef <yosefworku1221@gmail.com>
2023-05-02 18:40:39 +0300

    adds missing  work exprience

--- components/Experiences/index.tsx
+++ components/Experiences/index.tsx
@@ -50,7 +50,7 @@ export default function index({ innerRef }: any) {
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            Jan 2021 - Jun 2021
+            Jan 2022 - Jun 2022
           </time>
           <br />
           <a
@@ -67,7 +67,7 @@ export default function index({ innerRef }: any) {
             />
           </a>
           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Junior Software Developer
+            Software Developer
             <br />
             <ol className="pl-4 list-disc pt-2">
               <li>
@@ -81,6 +81,35 @@ export default function index({ innerRef }: any) {
               <li>
                 Participate in development of fleet driver&apos;s mobile app
               </li>
+            </ol>
+          </div>
+        </li>
+        <li className="mb-10 ml-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            May 2018 - Dec 2021
+          </time>
+          <br />
+          <a className="text-lg cursor-pointer hover:underline underline-offset-4 font-semibold text-blue-300 dark:text-white">
+            JoSystems {" "}
+            <FontAwesomeIcon
+              className="inline"
+              width={12}
+              icon={faExternalLink}
+            />
+          </a>
+          <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Android Developer
+            <br />
+            <ol className="pl-4 list-disc pt-2">
+              <li>
+                Being part of the team, I was responsible to create
+                user-friendly applications. designing the user interface,
+                implementing the application logic.
+              </li>
+              <li>Maintaining and updating existing applications</li>
+              <li>Gathering application Requirements</li>
+              <li>Integrating with back-end services and api </li>
             </ol>
           </div>
         </li>
