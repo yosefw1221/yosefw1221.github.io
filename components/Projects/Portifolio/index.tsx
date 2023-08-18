ae56ede1a335444f0425585a310c6af9a80c9337
Yosef <yosefworku1221@gmail.com>
2023-08-18 22:33:34 +0300

    fix : image scale issue on project card

--- components/Projects/Portifolio/index.tsx
+++ components/Projects/Portifolio/index.tsx
@@ -20,9 +20,9 @@ export default function PortfolioCard({
 }: PortfolioCardProps) {
   return (
     <div className='group text-white flex hover:drop-shadow-lg duration-500 transition-all hover:scale-105 flex-col max-w-sm m-3 bg-neutral-800 rounded-md drop-shadow-sm p-2 shadow-black'>
-      <div className='w-full h-56' >
+      <div className='w-full h-48' >
         <Image
-          className='rounded-t-md max-h-56 self-center'
+          className='rounded-t-md max-h-48 self-center object-cover'
           src={image}
           fill
           alt={title}
