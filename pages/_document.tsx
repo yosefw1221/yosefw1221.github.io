71aeff70eafc8d8fd3e4b292e1a03a88813e13c0
Yosef <yosefworku1221@gmail.com>
2023-08-18 22:34:47 +0300

    fix : removed unused font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Montserrat+Alternates:wght@400;600;700&family=Chakra+Petch:wght@400;600&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;700&display=swap'
           rel='stylesheet'
         />
       </Head>
